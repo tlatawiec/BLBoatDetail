53dcfe815a07718807021d5645c15ca655f5926e
tlatawiec <latawiecthomas@gmail.com>
2024-04-13 00:25:30 -0400

    recommit

--- app/quote/page.jsx
+++ app/quote/page.jsx
@@ -2,7 +2,7 @@ import Form from './form.jsx'
 
 export default function Pricing() {
   return (
-    <main>
+    <main className="mb-56">
       <title>Get a Quote</title>
       <h1 className="text-center text-5xl">GET A QUOTE</h1>
       <h2 className="text-center my-4 border-b w-1/2 mx-auto">Please fill out the information below, and our team will get back to you with a professional quote as soon as possible!</h2>

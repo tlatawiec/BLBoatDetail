b3717d02ec197404684abb54f76732556468b417
tlatawiec <latawiecthomas@gmail.com>
2024-04-14 15:45:51 -0400

    Call us to replace the email button

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,18 +5,12 @@ import "./globals.css";
 
 const dmsans = DM_Sans({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Brighton Boat Detail | Your Premier Boat Detailing Service in Barrington",
-  description: "Transform your boat with Brighton Boat Detail! We offer top-notch detailing services to keep your vessel shining on the water. Serving Barrington, our expert team ensures your boat maintains its pristine condition. Contact us today!",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/public/favicon.ico" sizes="any" />
       </head>
-
 
       <body className={dmsans.className}>
 
